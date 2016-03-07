e8354d6c97193a38dbc8ce0ae76fdaf11dc34341
klawson147 <kelbylawson@gmail.com>
2016-03-06 23:08:35 -0500

    Functioning File Dialogue

--- Bitmap_Processing/Bitmap_Processing/Source.cpp
+++ Bitmap_Processing/Bitmap_Processing/Source.cpp
@@ -1,31 +1,45 @@
-#include "Bitmap.h"
+﻿#include "Bitmap.h"
+#include "OpenFileDialog.h"
 #include <iostream>
 #include <fstream>
-#include <Windows.h>
+#include <windows.h>
 
 using namespace std;
 
-
 int main()
 {
-	int messageID = MessageBox(NULL, (LPCWSTR)L"Hello", (LPCWSTR)L"World", MB_OK);
-	cout << messageID << endl;
-	// Open BMP File
-	ifstream ifs("bmp3000.bmp", fstream::in | fstream::binary);
-	BMPHeader header;
-
-	// Pass File to be read
-	header.Fill(&ifs);
-
-	// Display Various Information
-	cout << header.toString();
+	// Open File Dialog
+	OpenFileDialog* openFileDialog1 = new OpenFileDialog();
+	openFileDialog1->ShowDialog();
 	
-	// Inverse Pixels
-	header.inversePixel();
+	// String Representation of Filename
+	string filename(openFileDialog1->FileName);
 
-	// Write BMP File
-	header.writeBMP();
+	// Open BMP File
+	ifstream ifs(filename, fstream::in | fstream::binary);
 
+	// Ensure File is still Accessible
+	if (!ifs.is_open())
+	{
+		cout << filename << " can't be opened\n";
+		
+	}
+	else
+	{
+		BMPHeader header;
+
+		// Pass File to be read
+		header.Fill(&ifs);
+
+		// Display Various Information
+		cout << header.toString();
+
+		// Inverse Pixels
+		header.inversePixel();
+
+		// Write BMP File
+		header.writeBMP();
+	}
+	
 	return 1;
 }
-
